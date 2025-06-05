c817d383a72979f8ef9e957617f5d58ffb2c1d84
TuonggVy <hongoctuongvy261104@gmail.com>
2025-06-05 11:39:56 +0700

    fix import

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { store } from "../redux/store";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import HomePage from "./pages/home/HomePage";
-import MainLayout from "./components/MainLayout";
+
 import { Provider } from "react-redux";
+import MainLayout from "./layouts/MainLayout";
 
 export default function App() {
   return (
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<MainLayout/>}>
             <Route index element={<HomePage />} />
             {/* Các route khác sẽ được thêm vào đây */}
           </Route>

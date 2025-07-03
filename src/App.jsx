262b7b382738d0e5ceed62e6729ebd9f890e79e4
Alex Bien <hoidev.codes@gmail.com>
2025-07-03 10:52:24 +0700

    Revert "Feature/recipe"

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
-import { Suspense, lazy } from "react";
+import React from "react";
+import { store } from "../redux/store";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Home from "./pages/home/Home";
 import { Provider } from "react-redux";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import MainLayout from "@layouts/MainLayout";
-import { store } from "@redux/store";
-
-// Lazy Load
-const HomePage = lazy(() => import("@/pages/HomePage"));
-const RecipePage = lazy(() => import("@pages/RecipePage"));
-const RecipeDetail = lazy(() => import("@components/Recipe/RecipeDetail"));
-const NotFoundPage = lazy(() => import("@pages/NotFoundPage"));
-const FavoritePage = lazy(() => import("@/pages/FavoritePage"));
-
 export default function App() {
   return (
     <Provider store={store}>
       <Router>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<MainLayout />}>
-              <Route index element={<HomePage />} />
-              <Route path="recipes" element={<RecipePage />} />
-              <Route path="recipes/:id" element={<RecipeDetail />} />
-              <Route path="favorites" element={<FavoritePage />} />
-            </Route>
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+        </Routes>
       </Router>
     </Provider>
   );
